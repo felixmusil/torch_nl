8c5de29fc59eff793dc7f5b2663aa281cad1e53d
felixmusil <felix.musil@epfl.ch>
2022-09-13 15:33:52 +0200

    n2 implementation works

---
+++ torch_nl/test_nl_n2.py
@@ -0,0 +1,79 @@
+import pytest
+import torch
+from ase.build import bulk, molecule
+import numpy as np
+from ase.neighborlist import neighbor_list
+
+from .ase_impl import ase_neighbor_list
+from .torch_impl import compute_nl_n2, compute_cell_shifts, compute_distances
+
+
+def bulk_metal():
+    frames = [
+        bulk('Si', 'diamond', a=6, cubic=True),
+        bulk("Cu", "fcc", a=3.6),
+        bulk('Si', 'diamond', a=6),
+    ]
+    return frames
+
+
+def atomic_structures():
+    frames = [
+        molecule("CH3CH2NH2"),
+        molecule("H2O"),
+        molecule("methylenecyclopropane"),
+    ] + bulk_metal() + [molecule('OCHCHO'), molecule('C3H9C'),]
+    return frames
+
+
+def ase2data(frames):
+    n_atoms = [0]
+    pos = []
+    cell = []
+    pbc = []
+    for ff in frames:
+        n_atoms.append(len(ff))
+        pos.append(torch.from_numpy(ff.get_positions()))
+        cell.append(torch.from_numpy(ff.get_cell().array))
+        pbc.append(torch.from_numpy(ff.get_pbc()))
+    pos = torch.cat(pos)
+    cell = torch.cat(cell)
+    pbc = torch.cat(pbc)
+    stride = torch.from_numpy(np.cumsum(n_atoms))
+    batch = torch.zeros(pos.shape[0],dtype=torch.long)
+    for ii,(st,nd) in enumerate(zip(stride[:-1],stride[1:])):
+        batch[st:nd] = ii
+    n_atoms = torch.Tensor(n_atoms[1:]).to(dtype=torch.long)
+    return pos, cell, pbc, batch, n_atoms
+
+@pytest.mark.parametrize(
+    "frames, cutoff, self_interaction",
+    [
+        (atomic_structures(), rc, self_interaction)
+        for rc in range(2, 6, 2)
+        for self_interaction in [True, False]
+    ],
+)
+def test_neighborlist(frames, cutoff, self_interaction):
+    """Check that torch_neighbor_list gives the same NL as ASE by comparing
+    the resulting sorted list of distances between neighbors."""
+    pos, cell, pbc, batch, n_atoms = ase2data(frames)
+
+
+    dds = []
+    mapping, batch_mapping, shifts_idx = compute_nl_n2(
+       cutoff, pos, cell, pbc, batch, self_interaction
+    )
+    cell_shifts = compute_cell_shifts(cell, shifts_idx, batch_mapping)
+    dds = compute_distances(pos, mapping, cell_shifts)
+    dds = np.sort(dds.numpy())
+
+    dd_ref = []
+    for frame in frames:
+        idx_i, idx_j, idx_S, dist = neighbor_list(
+            "ijSd", frame, cutoff=cutoff, self_interaction=self_interaction
+        )
+        dd_ref.extend(dist)
+    dd_ref = np.sort(dd_ref)
+
+    np.testing.assert_allclose(dd_ref, dds)
--- torch_nl/torch_impl.py
+++ torch_nl/torch_impl.py
@@ -1,109 +1,48 @@
-from typing import Tuple, Optional
+from typing import Optional
 import torch
 
-# @torch.jit.script
-def strides_of(v: torch.Tensor) -> torch.Tensor:
-    """TODO: add docs"""
-    strides = torch.zeros(v.shape[0] + 1, dtype=torch.int64, device=v.device)
-    strides[1:] = v
-    strides = torch.cumsum(strides, dim=0)
-    return strides
+from .image import compute_images
 
-def get_fully_connected_mapping(i_ids, j_ids) -> torch.Tensor:
-    mapping = torch.cartesian_prod(i_ids, j_ids)
-    mapping = mapping[mapping[0] != mapping[1], :]
-    return mapping
+def compute_distances(
+    pos: torch.Tensor,
+    mapping: torch.Tensor,
+    cell_shifts: Optional[torch.Tensor] = None,
+):
+    assert mapping.dim() == 2
+    assert mapping.shape[0] == 2
 
-# @torch.jit.script
-def compute_images(
-    positions: torch.Tensor,
-    cell: torch.Tensor,
-    pbc: torch.Tensor,
-    cutoff: float,
-    batch: torch.Tensor,
-    n_atoms: torch.Tensor,
-) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-    """TODO: add doc"""
+    if cell_shifts is None:
+        dr = pos[mapping[1]] - pos[mapping[0]]
+    else:
+        dr = pos[mapping[1]] - pos[mapping[0]] + cell_shifts
 
-    cell = cell.view((-1, 3, 3)).to(torch.float64)
-    pbc = pbc.view((-1, 3))
-    reciprocal_cell = torch.linalg.inv(cell).transpose(2, 1)
-    # print('reciprocal_cell: ', reciprocal_cell.device)
-    inv_distances = reciprocal_cell.norm(2, dim=-1)
-    # print('inv_distances: ', inv_distances.device)
-    num_repeats = torch.ceil(cutoff * inv_distances).to(torch.long)
-    num_repeats_ = torch.where(pbc, num_repeats, torch.zeros_like(num_repeats))
-    # print('num_repeats_: ', num_repeats_.device)
-    ids = torch.arange(positions.shape[0], device=positions.device, dtype=torch.long)
-    images, mapping, batch_images, shifts_expanded, shifts_idx_ = [], [], [], [], []
-    for i_structure in range(num_repeats_.shape[0]):
-        num_repeats = num_repeats_[i_structure]
-        r1 = torch.arange(
-            -num_repeats[0],
-            num_repeats[0] + 1,
-            device=cell.device,
-            dtype=torch.long,
-        )
-        r2 = torch.arange(
-            -num_repeats[1],
-            num_repeats[1] + 1,
-            device=cell.device,
-            dtype=torch.long,
-        )
-        r3 = torch.arange(
-            -num_repeats[2],
-            num_repeats[2] + 1,
-            device=cell.device,
-            dtype=torch.long,
-        )
-        shifts_idx = torch.cartesian_prod(r1, r2, r3)
-        shifts = torch.matmul(shifts_idx.to(cell.dtype), cell[i_structure])
-        pos = positions[batch == i_structure]
-        i_ids = ids[batch == i_structure].repeat(shifts.shape[0], 1)
-        
-        j_ids = i_ids.repeat(shifts.shape[0], 1)
-        s_mapping = get_fully_connected_mapping(i_ids, j_ids)
-        shift_expanded = shifts.repeat(1, n_atoms[i_structure]).view((-1, 3))
-        pos_expanded = pos.repeat(shifts.shape[0], 1)
+    return dr.norm(p=2, dim=1)
 
-        images.append(pos_expanded + shift_expanded)
-        mapping.append(s_mapping)
-        batch_images.append(
-            i_structure
-            * torch.ones(
-                images[-1].shape[0], dtype=torch.int64, device=cell.device
-            )
-        )
-        shifts_expanded.append(shift_expanded)
-        shifts_idx_.append(
-            shifts_idx.repeat(1, n_atoms[i_structure]).view((-1, 3))
-        )
-    return (
-        torch.cat(images, dim=0).to(positions.dtype),
-        torch.cat(mapping, dim=0).t(),
-        torch.cat(batch_images, dim=0),
-        torch.cat(shifts_expanded, dim=0).to(positions.dtype),
-        torch.cat(shifts_idx_, dim=0),
-    )
+def compute_cell_shifts(cell, shifts_idx, batch_mapping):
+    if cell is None:
+        cell_shifts = None
+    else:
+        cell_shifts = torch.einsum("jn,jnm->jm", shifts_idx, cell.view(-1, 3, 3)[batch_mapping])
+    return cell_shifts
 
-# @torch.jit.script
-def get_j_idx(
-    edge_index: torch.Tensor, batch_images: torch.Tensor, n_atoms: torch.Tensor
-) -> torch.Tensor:
-    """TODO: add docs"""
-    # get neighbor index reffering to the list of original positions
-    n_neighbors = torch.bincount(edge_index[0])
-    strides = strides_of(n_atoms)
-    n_reapeats = torch.zeros_like(n_atoms)
-    for i_structure, (st, nd) in enumerate(zip(strides[:-1], strides[1:])):
-        n_reapeats[i_structure] = torch.sum(n_neighbors[st:nd])
-    n_atoms = torch.repeat_interleave(n_atoms, n_reapeats, dim=0)
+def compute_strict_nl_n2(rcut, pos, cell, mapping, batch_mapping,  shifts_idx):
+    cell_shifts = compute_cell_shifts(cell, shifts_idx, batch_mapping)
+    if cell_shifts is None:
+        d2 = (pos[mapping[0]] - pos[mapping[1]]).square().sum(dim=1)
+    else:
+        d2 = (pos[mapping[0]] - pos[mapping[1]] - cell_shifts).square().sum(dim=1)
 
-    batch_i = torch.repeat_interleave(strides[:-1], n_reapeats, dim=0)
+    mask = d2 <= rcut*rcut
+    mapping = mapping[:, mask]
+    mapping_batch = batch_mapping[mask]
+    shifts_idx = shifts_idx[mask]
+    return mapping, mapping_batch, shifts_idx # , d2[mask].sqrt()
 
-    n_images = torch.bincount(batch_images)
-    strides_images = strides_of(n_images[:-1])
-    images_shift = torch.repeat_interleave(strides_images, n_reapeats, dim=0)
 
-    j_idx = torch.remainder(edge_index[1] - images_shift, n_atoms) + batch_i
-    return j_idx
+def compute_nl_n2(rcut, pos, cell, pbc, batch, self_interaction):
+    n_atoms = torch.bincount(batch)
+    mapping, batch_mapping, shifts_idx = compute_images(pos,
+                                     cell, pbc, rcut,
+                                     n_atoms, self_interaction)
+    mapping, mapping_batch, shifts_idx = compute_strict_nl_n2(rcut, pos, cell, mapping, batch_mapping,  shifts_idx)
+    return mapping, mapping_batch, shifts_idx
