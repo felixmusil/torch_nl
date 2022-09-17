7382b021c762c57b4e292498c69b24b350bc162c
felixmusil <felix.musil@epfl.ch>
2022-09-17 11:41:41 +0200

    aa

---
+++ profile.py
@@ -0,0 +1,36 @@
+import torch.profiler
+import torch
+
+torch.jit.set_fusion_strategy([("STATIC", 3), ("DYNAMIC", 3)])
+
+
+import numpy as np
+
+from torch_nl import compute_neighborlist, compute_neighborlist_n2, ase2data
+from torch_nl.timer import timeit
+
+from ase.build import molecule, bulk, make_supercell
+
+device = 'cpu'
+cutoff = 4
+frame = bulk('Si', 'diamond', a=4, cubic=True)
+
+frame = make_supercell(frame, 5*np.eye(3))
+
+
+
+pos, cell, pbc, batch, n_atoms = ase2data([frame], device=device)
+
+
+with torch.profiler.profile(
+            schedule=torch.profiler.schedule(
+                wait=20, warmup=20, active=2, repeat=1
+            ),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler('nl_n.prof'),
+            record_shapes=False,
+            profile_memory=False,
+            with_stack=True,
+        ) as prof:
+    for _ in range(50):
+        mapping, mapping_batch, shifts_idx = compute_neighborlist(cutoff, pos, cell, pbc, batch)
+        prof.step()
