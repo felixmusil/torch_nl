540963bc2000944102d6a964884b4ee6da2933c5
felixmusil <felix.musil@epfl.ch>
2022-10-03 21:21:18 +0200

    add benchmark file

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -7,23 +7,26 @@ from ase.build import molecule, bulk, make_supercell
 from ase.neighborlist import neighbor_list
 import pandas as pd
 from tqdm import tqdm
+
 # import seaborn as sns
 import matplotlib.pyplot as plt
-sys.path.insert(0,'../')
+
+sys.path.insert(0, "../")
 from torch_nl import compute_neighborlist, compute_neighborlist_n2, ase2data
 from torch_nl.timer import timeit
+
 torch.set_num_threads(4)
 cutoff = 4
 tags = [
     # "torch_nl O(n^2) CPU",
     "torch_nl O(n^2) GPU",
     "torch_nl O(n) CPU",
-    # "torch_nl O(n) GPU"
+    "torch_nl O(n) GPU"
 ]
 
-frame = bulk('Si', 'diamond', a=4, cubic=True)
+frame = bulk("Si", "diamond", a=4, cubic=True)
 aa = torch.arange(1, 6)
-Ps = torch.cartesian_prod(aa,aa,aa)
+Ps = torch.cartesian_prod(aa, aa, aa)
 Ps = Ps[torch.sort(Ps.sum(dim=1)).indices].to(torch.long).numpy()
 frames = []
 n_atoms = []
@@ -35,33 +38,43 @@ print("Starting")
 tag = "ASE"
 datas = []
 for frame in tqdm(frames):
-    timing = timeit(neighbor_list, ['ijS', frame, cutoff], tag=tag, warmup=1, nit=50)
+    timing = timeit(
+        neighbor_list, ["ijS", frame, cutoff], tag=tag, warmup=1, nit=50
+    )
     data = timing.dumps()
-    i,j,S = neighbor_list('ijS', frame, cutoff)
+    i, j, S = neighbor_list("ijS", frame, cutoff)
     n_neighbor = np.bincount(i).mean()
     data.update(n_atom=len(frame), n_neighbor_per_atom_avg=int(n_neighbor))
-    data.pop('samples')
+    data.pop("samples")
     datas.append(data)
 
 
 for tag in tqdm(tags):
     if "CPU" in tag:
-        device = 'cpu'
+        device = "cpu"
     elif "GPU" in tag:
-        device = 'cuda'
+        device = "cuda"
 
-    if 'O(n^2)' in tag:
+    if "O(n^2)" in tag:
         nl_func = compute_neighborlist_n2
-    elif 'O(n)' in tag:
+    elif "O(n)" in tag:
         nl_func = compute_neighborlist
 
     for frame in tqdm(frames):
         pos, cell, pbc, batch, n_atoms = ase2data([frame], device=device)
         with torch.cuda.amp.autocast():
-            timing = timeit(nl_func, [cutoff, pos, cell, pbc, batch], tag=tag, warmup=10, nit=50)
+            timing = timeit(
+                nl_func,
+                [cutoff, pos, cell, pbc, batch],
+                tag=tag,
+                warmup=10,
+                nit=50,
+            )
         data = timing.dumps()
-        data.pop('samples')
-        mapping, mapping_batch, shifts_idx = nl_func(cutoff, pos, cell, pbc, batch)
+        data.pop("samples")
+        mapping, mapping_batch, shifts_idx = nl_func(
+            cutoff, pos, cell, pbc, batch
+        )
         n_neighbor = np.bincount(mapping[0].cpu().numpy()).mean()
         data.update(n_atom=len(frame), n_neighbor_per_atom_avg=int(n_neighbor))
         datas.append(data)
